5be0d6135773798e807155a41db647c6a8a836b4
Bram Ravijts <bram.ravijts@gmail.com>
2019-04-24 22:29:21 +0200

    dashboard

--- index.php
+++ index.php
@@ -4,9 +4,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Poppins|Ubuntu" rel="stylesheet"> 
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/dashboard.css">
     <title>Dashboard - Be ware, Be water</title>
 </head>
 <body>
+    <div id="dashboardContainer">
+        <div class="dashboardDiv" id="usage">
+            <h1 id="currentUsage">100 <span id="liter">L</span></h1>
+            <img src="#" alt="graph" id="graph">
+        </div>
+        <div class="dashboardDiv" id="add">
+            <h2>Apparaat toevoegen</h2>
+            <a href="#" id="addButton"></a>
+        </div>
+        <div class="dashboardDiv" id="ranking">
+            <h2>Grootste verbruikers</h2>
+                <p class="rankName">Toilet <span class="rankUsage">50L</span></p>
+                <p class="rankName">Toilet 2 <span class="rankUsage">40L</span></p>
+                <p class="rankName">Douche <span class="rankUsage">30L</span></p>
+                <p class="rankName">Bad <span class="rankUsage">25L</span></p>
+                <p class="rankName">Afwasmachine <span class="rankUsage">20L</span></p>
+                <p class="rankName">Wasmachine <span class="rankUsage">20L</span></p>
+            
+        </div>
+        <div class="dashboardDiv" id="pay">
+            <h2>Te betalen</h2>
+            <h2>€99</h2>
+        </div>
+    </div>
     
 </body>
 </html>
