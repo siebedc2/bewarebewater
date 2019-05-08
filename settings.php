b1a95bbdd1d7877c09bc6f43cd3d7822f5b66f15
Siebe <siebedc2@gmail.com>
2019-05-08 12:01:12 +0200

    Created pages

---
+++ settings.php
@@ -0,0 +1,40 @@
+<?php
+    
+
+
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="icon" href="images/logo.png">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Poppins" rel="stylesheet">
+    <title>Familie</title>
+</head>
+
+<body>
+    <div class="overlay"></div>
+
+    <header>
+        <?php include("nav.inc.php");?>
+    </header>
+
+    <!-- SHOW data from DB -->
+    <div class="settings">
+        <h2>Instellingen</h2>
+
+      
+    </div>
+
+    <a href="#" class="btn btnSecondary">Opslaan</a>
+
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
