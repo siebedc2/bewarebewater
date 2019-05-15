f7f206a69b895f613e0b5261b9dc5b35991cc8ac
Siebe <siebedc2@gmail.com>
2019-05-15 15:11:20 +0200

    added a function to get al members

--- family.php
+++ family.php
@@ -1,6 +1,12 @@
 <?php
     
+    require_once("bootstrap/bootstrap.php");
 
+    $userId = User::getUserId();
+
+    //var_dump($userId);
+
+    $results = User::getAllMembers($userId);
 
 
 ?><!DOCTYPE html>
@@ -35,8 +41,15 @@
        </div>
 
        <?php //foreach ($results as $result): ?>
-            <div class="user">
-                <!--<p><?php echo $result['firstname'] . " " . $result['lastname']; ?></p>-->
+            <!-- Echo the profile picture of the person-->
+            <!--<div class="familyMember">
+                <div class="profileImg">
+                    <img src="images/profilePictures/profile.jpg" alt="Profile image">
+                    <div class="backimg"></div>
+                </div>-->
+
+                <!-- Echo firstname and lastname of member -->
+                <p><?php // echo $result['firstname']?></p>
             </div>
         <?php //endforeach; ?>
 
