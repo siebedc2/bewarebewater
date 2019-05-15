10eb30b1bcdf291bcea9cbe4344d5cc321d7ffed
Siebe <siebedc2@gmail.com>
2019-05-15 20:20:09 +0200

    Added function to add a device to you home

--- classes/Device.php
+++ classes/Device.php
@@ -1,7 +1,14 @@
 <?php 
     class Device {
-        public static function addDevice($userId) {
-            
+        public static function addDevice($userId, $room, $device) {
+            $conn = Db::getConnection();
+            $statement = $conn->prepare("insert into device (name, room, user_id) values (:device, :room, :userId)");
+            $statement->bindParam(":device", $device);
+            $statement->bindParam(":room", $room);
+            $statement->bindParam(":userId", $userId);
+            $statement->execute();
+            $results = $statement->fetchAll(PDO::FETCH_ASSOC);
+            return $results;
         }
 
     }
